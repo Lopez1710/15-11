3296cc489662294c0b0ba5d0283eab4fa99699a5
Lopez1710 <69539735+Lopez1710@users.noreply.github.com>
2022-11-15 11:36:04 -0600

    final

--- DrawerBottomViewApp1/app/src/main/java/com/example/drawerbottomviewapp1/ButtonActivity.kt
+++ DrawerBottomViewApp1/app/src/main/java/com/example/drawerbottomviewapp1/ButtonActivity.kt
@@ -2,10 +2,37 @@ package com.example.drawerbottomviewapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ButtonActivity : AppCompatActivity() {
+    private lateinit var btmenu:BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_button)
+
+        var inicioFragment = InicioFragment()
+        var mensajeFragment = mensajeFragment()
+        var perfilFragment = perfilFragment()
+
+        btmenu=findViewById(R.id.bnv_Menu)
+        btmenu.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.myinicio->{
+                    setfragmentactual(inicioFragment)
+                }
+                R.id.mymensaje->{
+                    setfragmentactual(mensajeFragment)
+                }
+                R.id.myperfil->{
+                    setfragmentactual(perfilFragment)
+                }
+            }
+        }
+    }
+
+    private fun setfragmentactual(fragment:Fragment)=supportFragmentManager.beginTransaction().apply{
+        replace(R.id.fl_vista,fragment)
+        commit()
     }
 }
